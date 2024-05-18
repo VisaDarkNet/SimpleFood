66daf4acab50fe67d00567c101269967d9b5fcdd
visadarknet <visadarknet@gmail.com>
2024-05-18 21:14:55 +0500

    адаптив

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,37 @@
 $(function () {
   $('.reviews__box').slick({
     dots: true,
-    prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/icon-prev-arrow.svg" alt="arrow-left"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="images/icons/icon-prev-arrow.svg" alt="arrow-right"></button>',
+    infinite: false,
+    prevArrow: '<button type="button" class="reviews__slick-prev"> <svg class="reviews__icon"><use xlink:href="images/sprite.svg#icon-prev-arrow"></use></svg> </button>',
+    nextArrow: '<button type="button" class="reviews__slick-next"> <svg class="reviews__icon"><use xlink:href="images/sprite.svg#icon-prev-arrow"></use></svg> </button>',
+    appendArrows: '.reviews__arrows',
+
   });
+});
+
+$(window).on('load resize', function () {
+  if ($(window).width() < 576) {
+    $('.restaurant__box:not(.slick-initialized)').slick({
+      arrows: false,
+      dots: true,
+      infinite: true,
+      speed: 100,
+      slidesToShow: 1
+    });
+  } else {
+    $(".restaurant__box.slick-initialized").slick("unslick");
+  }
+});
+
+const burger = document.querySelector('.burger');
+const menuList = document.querySelector('.menu');
+const body = document.querySelector('body')
+
+burger.addEventListener('click', function () {
+  this.classList.toggle('active');
+  menuList.classList.toggle('menu__list--active');
+  body.classList.toggle('lock');
+
 });
 
 var mixer = mixitup('.popular-categories__content');
